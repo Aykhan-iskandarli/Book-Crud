b1b4e5f89020c020f6b38eff02c9efb81eda5479
Aykhan <39117952+Aykhan-iskandarli@users.noreply.github.com>
2022-03-17 12:13:03 +0400

    updated

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Gif from "../../assets/images/loading.gif"
 
 const Loading = () => {
   return (
     <div className='loading'>
         <div className="loading-img">
-            <img src="./assets/images/loading.gif" alt="" />
+            <img src={Gif} alt="" />
         </div>
     </div>
   )
